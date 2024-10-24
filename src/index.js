a973637e50fe124a793a852072ab4d661b452b23
Boris Drokin <drokcin.b@gmail.com>
2024-10-24 22:42:28 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pairs = [];
+  
+  for (let pair of bracketsConfig) {
+      pairs.push(pair.join(""));
+  }
+
+  let chars = '';
+  
+  while (str != chars) {
+    chars = str;
+    
+    pairs.forEach(element => {
+      str = str.replace(element,"");
+    });
+  }
+  
+  str.length > 0 ? chars = false : chars = true;
+  
+  return(chars);
 }
